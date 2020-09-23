b6ee37df822e7fc371bdca5036f6e3d6fb1ead8b
Pedro Melo <ppvitorr@gmail.com>
2020-09-23 20:59:16 -0300

    Create PessoaDaoTest

---
+++ src/test/java/dao/PessoaDAOTest.java
@@ -0,0 +1,30 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import domain.Pessoa;
+import util.CpfUtil;
+
+class PessoaDAOTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "\n\n003\n005\n006\n007\n009\n010", "003\n005\n006\n007\n009\n010\n\n\n",
+			"003\n005\n006\n007\n009\n010", "\n\n\n\n003\n005\n006\n007\n009\n010\n\n\n\n\n" })
+	void testBuscarPorListaDeCPF(String listaDeCpf) {
+		String listaFormatada = CpfUtil.ajustarListaDeCpf(listaDeCpf);
+		PessoaDAO dao = new PessoaDAO();
+		List<Pessoa> pessoas = dao.buscarPorListaDeCPF(listaFormatada);
+		Integer[] arrayIds = new Integer[pessoas.size()];
+		for(int i = 0; i < pessoas.size(); i++) {
+			arrayIds[i] = pessoas.get(i).getIdent();
+		}
+		Integer[] esperado = {1, 3, 4, 5, 8};
+		assertArrayEquals(esperado, arrayIds);
+	}
+
+}
