a01a40d3d2c1c38faea289bc40967d72e374380a
Pedro Melo <ppvitorr@gmail.com>
2020-09-24 21:21:59 -0300

    Update GenericDomain
    Remove unused methods

--- src/main/java/domain/GenericDomain.java
+++ src/main/java/domain/GenericDomain.java
@@ -12,41 +12,7 @@ public class GenericDomain implements Serializable {
 	@Id
 	private Integer ident;
 	
-	@Override
-	public String toString() {
-	    return String.format("%s[id=%d]", getClass().getSimpleName(), getIdent());
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((ident == null) ? 0 : ident.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GenericDomain other = (GenericDomain) obj;
-		if (ident == null) {
-			if (other.ident != null)
-				return false;
-		} else if (!ident.equals(other.ident))
-			return false;
-		return true;
-	}
-
 	public Integer getIdent() {
 		return ident;
-	}
-
-	public void setIdent(Integer ident) {
-		this.ident = ident;
 	}
 }
