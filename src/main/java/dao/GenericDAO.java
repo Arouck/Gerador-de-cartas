59ba835e888cd9bd47608d9ca685e8575dc88b98
Pedro Melo <ppvitorr@gmail.com>
2020-09-24 21:22:12 -0300

    Update GenericDAO
    Remove unused methods

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -1,7 +1,5 @@
 package dao;
 
-import java.lang.reflect.ParameterizedType;
-
 import javax.persistence.EntityManager;
 
 import util.EntityManagerUtil;
@@ -9,12 +7,8 @@ import util.EntityManagerUtil;
 public class GenericDAO<Entidade> {
 
 	protected EntityManager em;
-	private Class<Entidade> classe;
 	
-	@SuppressWarnings("unchecked")
 	public GenericDAO() {
-		this.classe = (Class<Entidade>) ((ParameterizedType) getClass().getGenericSuperclass())
-				.getActualTypeArguments()[0];
 		instanciarEm();
 	}
 	
@@ -22,32 +16,15 @@ public class GenericDAO<Entidade> {
 		em.getTransaction().begin();
 	}
 	
-	public void fazerCommmit() {
-		em.getTransaction().commit();
-	}
-	
 	public void fecharSessao() {
 		em.close();
-	}
-	
-	public void fazerRollback() {
-		em.getTransaction().rollback();
 	}
 	
 	public void instanciarEm() {
 		setEm(EntityManagerUtil.getEntityManager());
 	}
 	
-	public EntityManager getEm() {
-		return em;
-	}
 	public void setEm(EntityManager em) {
 		this.em = em;
-	}
-	public Class<Entidade> getClasse() {
-		return classe;
-	}
-	public void setClasse(Class<Entidade> classe) {
-		this.classe = classe;
 	}
 }
