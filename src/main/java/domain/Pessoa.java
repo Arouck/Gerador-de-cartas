09b994a80de071c94d82581765e93bc56e222d9b
Pedro Melo <ppvitorr@gmail.com>
2020-09-24 21:21:44 -0300

    Update Pessoa domain
    Remove unused methods

--- src/main/java/domain/Pessoa.java
+++ src/main/java/domain/Pessoa.java
@@ -41,30 +41,6 @@ public class Pessoa extends GenericDomain {
 	@Transient
 	private String mensagemErro;
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
 	public String getCep() {
 		return cep;
 	}
@@ -72,51 +48,11 @@ public class Pessoa extends GenericDomain {
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-
-	public String getNumeroendereco() {
-		return numeroendereco;
-	}
-
-	public void setNumeroendereco(String numeroendereco) {
-		this.numeroendereco = numeroendereco;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getOperacao() {
-		return operacao;
-	}
-
-	public void setOperacao(String operacao) {
-		this.operacao = operacao;
-	}
-
+	
 	public String getUf() {
 		return uf;
 	}
-
+	
 	public void setUf(String uf) {
 		this.uf = uf;
 	}
