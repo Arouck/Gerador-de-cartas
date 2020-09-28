01384d443a2be79f6474ddbbe2113a2dfc4e63ab
Pedro Melo <ppvitorr@gmail.com>
2020-09-28 20:07:43 -0300

    Implemented PDFUtil to generate documents

--- src/main/java/util/PDFUtil.java
+++ src/main/java/util/PDFUtil.java
@@ -1,5 +1,41 @@
 package util;
 
+import java.io.File;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import domain.Pessoa;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+
 public class PDFUtil {
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void gerarDocumento(List<Pessoa> pessoas, String path) {
+		try {
+			
+			URL jasperPath = PDFUtil.class.getClassLoader().getResource("Modelo_Cartas.jasper");
+			File file = new File(jasperPath.toURI());
+
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getAbsolutePath());
+
+			JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(pessoas);
+
+			Map parameters = new HashMap<>();
+			parameters.put("ds", ds);
+
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, ds);
+
+			JasperExportManager.exportReportToPdfFile(jasperPrint, path);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
