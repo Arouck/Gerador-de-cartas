76441c4cc0bc83f82279f6b2f984cbb4a92104be
Pedro Melo <ppvitorr@gmail.com>
2020-09-30 14:29:08 -0300

    Create a new method to save the file using the terminal
    And another using the web browser

--- src/main/java/util/XLSXUtil.java
+++ src/main/java/util/XLSXUtil.java
@@ -21,66 +21,118 @@ import controller.FileController;
 import domain.Pessoa;
 
 public class XLSXUtil {
-	
+
 	private static final String[] columns = { "Nome", "CEP", "Logradouro", "Complemento", "Numero", "Bairro", "Cidade",
 			"Estado", "Menssagem de Erro" };
-	
-	public static void gerarTabelaExcel(List<Pessoa> pessoas) throws IOException {
+
+	public static void gerarTabelaExcelFileDialog(List<Pessoa> pessoas) throws IOException {
 		Workbook workbook = new XSSFWorkbook();
-		
+
 		Sheet sheet = workbook.createSheet("Lista de pessoas");
 		Font headerFont = workbook.createFont();
-		
+
 		headerFont.setBold(true);
 		headerFont.setFontHeightInPoints((short) 15);
 		headerFont.setColor(IndexedColors.GREY_25_PERCENT.getIndex());
-		
+
 		CellStyle headerCellStyle = workbook.createCellStyle();
 		headerCellStyle.setFont(headerFont);
-		
+
 		Row headerRow = sheet.createRow(0);
-		
+
 		for (int i = 0; i < columns.length; i++) {
 			Cell cell = headerRow.createCell(i);
 			cell.setCellValue(columns[i]);
 			cell.setCellStyle(headerCellStyle);
 		}
-		
+
 		int rowNumber = 1;
-		
-		 for (Pessoa pessoa : pessoas) {
-		 	Row row = sheet.createRow(rowNumber++);
-		 	
-		 	if (pessoa.getComplemento() == null || pessoa.getComplemento().isEmpty()) {
-		 		pessoa.setComplemento(" ");
-		 	}
-		 	
-		 	if (pessoa.getNumeroendereco() == null || pessoa.getNumeroendereco().isEmpty()) {
-		 		pessoa.setNumeroendereco(" ");
-		 	}
-			
-		 	row.createCell(0).setCellValue(pessoa.getNome());
-		 	row.createCell(1).setCellValue(pessoa.getCep());
-		 	row.createCell(2).setCellValue(pessoa.getEndereco());
-		 	row.createCell(3).setCellValue(pessoa.getComplemento());
-		 	row.createCell(4).setCellValue(pessoa.getNumeroendereco());
-		 	row.createCell(5).setCellValue(pessoa.getBairro());
-		 	row.createCell(6).setCellValue(pessoa.getCidade());
-		 	row.createCell(7).setCellValue(pessoa.getUf());
-		 	row.createCell(8).setCellValue(pessoa.getMensagemErro());
+
+		for (Pessoa pessoa : pessoas) {
+			Row row = sheet.createRow(rowNumber++);
+
+			if (pessoa.getComplemento() == null || pessoa.getComplemento().isEmpty()) {
+				pessoa.setComplemento(" ");
+			}
+
+			if (pessoa.getNumeroendereco() == null || pessoa.getNumeroendereco().isEmpty()) {
+				pessoa.setNumeroendereco(" ");
+			}
+
+			row.createCell(0).setCellValue(pessoa.getNome());
+			row.createCell(1).setCellValue(pessoa.getCep());
+			row.createCell(2).setCellValue(pessoa.getEndereco());
+			row.createCell(3).setCellValue(pessoa.getComplemento());
+			row.createCell(4).setCellValue(pessoa.getNumeroendereco());
+			row.createCell(5).setCellValue(pessoa.getBairro());
+			row.createCell(6).setCellValue(pessoa.getCidade());
+			row.createCell(7).setCellValue(pessoa.getUf());
+			row.createCell(8).setCellValue(pessoa.getMensagemErro());
 		}
-		
+
 		for (int j = 0; j < columns.length; j++) {
 			sheet.autoSizeColumn(j);
 		}
-		
-		FileController.saveXLSXFile(workbook);
-		
+
+		FileController.saveXLSXFileDialog(workbook);
 	}
-	
+
+	public static void gerarTabelaExcel(List<Pessoa> pessoas) throws IOException {
+		Workbook workbook = new XSSFWorkbook();
+
+		Sheet sheet = workbook.createSheet("Lista de pessoas");
+		Font headerFont = workbook.createFont();
+
+		headerFont.setBold(true);
+		headerFont.setFontHeightInPoints((short) 15);
+		headerFont.setColor(IndexedColors.GREY_25_PERCENT.getIndex());
+
+		CellStyle headerCellStyle = workbook.createCellStyle();
+		headerCellStyle.setFont(headerFont);
+
+		Row headerRow = sheet.createRow(0);
+
+		for (int i = 0; i < columns.length; i++) {
+			Cell cell = headerRow.createCell(i);
+			cell.setCellValue(columns[i]);
+			cell.setCellStyle(headerCellStyle);
+		}
+
+		int rowNumber = 1;
+
+		for (Pessoa pessoa : pessoas) {
+			Row row = sheet.createRow(rowNumber++);
+
+			if (pessoa.getComplemento() == null || pessoa.getComplemento().isEmpty()) {
+				pessoa.setComplemento(" ");
+			}
+
+			if (pessoa.getNumeroendereco() == null || pessoa.getNumeroendereco().isEmpty()) {
+				pessoa.setNumeroendereco(" ");
+			}
+
+			row.createCell(0).setCellValue(pessoa.getNome());
+			row.createCell(1).setCellValue(pessoa.getCep());
+			row.createCell(2).setCellValue(pessoa.getEndereco());
+			row.createCell(3).setCellValue(pessoa.getComplemento());
+			row.createCell(4).setCellValue(pessoa.getNumeroendereco());
+			row.createCell(5).setCellValue(pessoa.getBairro());
+			row.createCell(6).setCellValue(pessoa.getCidade());
+			row.createCell(7).setCellValue(pessoa.getUf());
+			row.createCell(8).setCellValue(pessoa.getMensagemErro());
+		}
+
+		for (int j = 0; j < columns.length; j++) {
+			sheet.autoSizeColumn(j);
+		}
+
+		FileController.saveXLSXFile(workbook);
+
+	}
+
 	public static List<Pessoa> lerArquivoXLSX() throws IOException {
 		List<Pessoa> pessoas = new ArrayList<>();
-		
+
 		File file = FileController.openXLSXFile();
 		FileInputStream fileInput = new FileInputStream(file);
 
@@ -96,9 +148,9 @@ public class XLSXUtil {
 
 			Iterator<Cell> cells = row.cellIterator();
 
-			Pessoa pessoa = new Pessoa(cells.next().toString(), cells.next().toString(),
+			Pessoa pessoa = new Pessoa(cells.next().toString(), cells.next().toString(), cells.next().toString(),
 					cells.next().toString(), cells.next().toString(), cells.next().toString(), cells.next().toString(),
-					cells.next().toString(), cells.next().toString());
+					cells.next().toString());
 
 			pessoas.add(pessoa);
 		}
