8ea93257c2d77cca987c279a735a112ac7072f69
Pedro Melo <ppvitorr@gmail.com>
2020-09-30 14:29:38 -0300

    Create a new method to save the file using terminal and another using
    Web browser

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -2,18 +2,26 @@ package controller;
 
 import java.awt.FileDialog;
 import java.awt.Frame;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 import org.apache.poi.ss.usermodel.Workbook;
+import org.omnifaces.util.Faces;
 
 public class FileController {
-
-	public static void saveXLSXFile(Workbook workbook) {
+	
+	public static void saveXLSXFileDialog(Workbook workbook) throws IOException {
 		FileDialog fileDialog = new FileDialog(new Frame(), "Salvar", FileDialog.SAVE);
 		fileDialog.setFile("*.xlsx;*.xls");
 		fileDialog.setVisible(true);
+		
 		File file = null;
 		String path = "";
 		
@@ -22,17 +30,46 @@ public class FileController {
 		} else {
 			path = fileDialog.getFiles()[0].getAbsolutePath();
 		}
-
+		
 		file = new File(path);
-
+		
 		try {
 			FileOutputStream fileOutput = new FileOutputStream(file.getAbsolutePath());
 			workbook.write(fileOutput);
 			fileOutput.close();
+			workbook.write(fileOutput);
 			workbook.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void saveXLSXFile(Workbook workbook) throws IOException {
+		LocalDate date = LocalDate.now();
+		//File file = null;
+		String defaultFileName = "Tabela " + date.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt")) + " " + date.getYear() + ".xlsx";
+		//File file = new File("C:\\Users\\pvito\\Documents\\" + defaultFileName);
+		//String path = "%USERPROFILE%\\Documents\\Cartas\\" + date.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt")) + "\\Tabela.xlsx";
+		
+		/**/
+
+		//
+		
+		/*FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		
+		externalContext.responseReset();
+		externalContext.setResponseContentType("application/vnd.ms-excel");
+	    externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"" + defaultFileName + "\"");
+
+		*/
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        workbook.write(bos);
+        byte[] barray = bos.toByteArray();
+        InputStream is = new ByteArrayInputStream(barray);
+		
+		Faces.sendFile(is, defaultFileName, true);
 	}
 	
 	public static String savePDFFile() {
