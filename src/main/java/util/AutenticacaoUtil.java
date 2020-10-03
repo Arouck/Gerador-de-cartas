e301f9515caa63f850302baaacfbcb8cf9eef911
Pedro Melo <ppvitorr@gmail.com>
2020-10-03 01:20:45 -0300

    Update AutenticacaoUtil

--- src/main/java/util/AutenticacaoUtil.java
+++ src/main/java/util/AutenticacaoUtil.java
@@ -9,24 +9,24 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
-import org.apache.shiro.codec.Hex;
+import org.apache.commons.codec.binary.Hex;
 
 public class AutenticacaoUtil {
 
-	public static String criptografarSenha(String password, String salt, int iterations, int keyLength)
+	public static String criptografarSenha(String password, String salt)
 			throws GeneralSecurityException {
 		try {
+			int iterations = 5000;
+			int keyLength = 512;
 			char[] bytePassword = password.toCharArray();
 			String pepper = "?UJ?G\\A:EXA2DW+}ABD[";
 			salt += pepper;
 			byte[] byteSalt = salt.getBytes();
 			SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
 			PBEKeySpec pbeKeySpec = new PBEKeySpec(bytePassword, byteSalt, iterations, keyLength);
-			// PBEKeySpec pbeKeySpec = new PBEKeySpec(password, salt, iterationCount,
-			// keyLength)
 			SecretKey key = secretKeyFactory.generateSecret(pbeKeySpec);
 			byte[] bytesCriptografados = key.getEncoded();
-			String senhaCriptografada = Hex.encodeToString(bytesCriptografados);
+			String senhaCriptografada = Hex.encodeHexString(bytesCriptografados);
 			return senhaCriptografada;
 		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
 			e.printStackTrace();
