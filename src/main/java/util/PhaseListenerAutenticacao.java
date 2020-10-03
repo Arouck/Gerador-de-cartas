c153fcb3716868521ab0819e809899761ffe3b49
Pedro Melo <ppvitorr@gmail.com>
2020-10-03 01:20:27 -0300

    Create PhaseListener

---
+++ src/main/java/util/PhaseListenerAutenticacao.java
@@ -0,0 +1,37 @@
+package util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import org.omnifaces.util.Faces;
+
+import bean.AutenticacaoBean;
+
+@SuppressWarnings("serial")
+public class PhaseListenerAutenticacao implements PhaseListener {
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		String paginaAtual = Faces.getViewId();
+		
+		if(!paginaAtual.contains("login.xhtml")) {
+			AutenticacaoBean autenticacaoBean = Faces.getSessionAttribute("autenticacaoBean");
+			if (autenticacaoBean != null && autenticacaoBean.getUsuarioLogado() != null) {
+				
+			} else {
+				Faces.navigate("/pages/login.xhtml?faces-redirect=true");				
+			}
+		}
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+
+}
