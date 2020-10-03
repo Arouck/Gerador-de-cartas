b9a9ccf75d99c8b5e3eb3e6671f60cf320786afa
Pedro Melo <ppvitorr@gmail.com>
2020-10-02 23:09:01 -0300

    Update PDFUtil

--- src/main/java/util/PDFUtil.java
+++ src/main/java/util/PDFUtil.java
@@ -1,10 +1,17 @@
 package util;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import org.omnifaces.util.Faces;
 
 import domain.Pessoa;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -17,11 +24,11 @@ import net.sf.jasperreports.engine.util.JRLoader;
 public class PDFUtil {
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void gerarDocumento(List<Pessoa> pessoas, String path) {
+	public static void gerarDocumento(List<Pessoa> pessoas) {
 		try {
 			
 			URL jasperPath = PDFUtil.class.getClassLoader().getResource("Modelo_Cartas.jasper");
-			File file = new File(jasperPath.toURI());
+			File file = new File(jasperPath.getFile());
 
 			JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getAbsolutePath());
 
@@ -32,7 +39,14 @@ public class PDFUtil {
 
 			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, ds);
 
-			JasperExportManager.exportReportToPdfFile(jasperPrint, path);
+			//JasperExportManager.exportReportToPdfFile(jasperPrint);
+			byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+			
+			LocalDate date = LocalDate.now();
+			String defaultFileName = "Cartas-" + date.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt")) + "-"
+					+ date.getYear() + ".pdf";
+			InputStream inputStream = new ByteArrayInputStream(pdf);
+			Faces.sendFile(inputStream, defaultFileName, true);
 
 		} catch (Exception e) {
 			e.printStackTrace();
