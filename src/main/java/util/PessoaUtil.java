4e903dd654829a32731b27c686227f1c37902806
Pedro Melo <ppvitorr@gmail.com>
2020-10-07 10:54:24 -0300

    Update PessoaUtil
    Update formatarBairro and formatarCep to include invalid cpf message on excel. Create toList method

--- src/main/java/util/PessoaUtil.java
+++ src/main/java/util/PessoaUtil.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +11,7 @@ import java.util.regex.Pattern;
 import domain.Pessoa;
 
 public class PessoaUtil {
-	
+
 	public static List<Pessoa> formatarEnderecoCompleto(List<Pessoa> pessoas) {
 		pessoas = PessoaUtil.formatarCep(pessoas);
 		List<Boolean> validador = new ArrayList<>();
@@ -24,13 +25,13 @@ public class PessoaUtil {
 			}
 			ceps.add(pessoa.getCep());
 		}
-		
+
 		List<String> enderecosJson = ClienteViaCepWS.buscarCep(ceps, validador);
 		List<Map<String, String>> enderecosFormatados = PessoaUtil.formatarJsonViaCep(enderecosJson);
 
 		pessoas = formatarBairro(pessoas, enderecosFormatados);
 		pessoas = preencherUF(pessoas, enderecosFormatados);
-		
+
 		return pessoas;
 	}
 
@@ -46,13 +47,15 @@ public class PessoaUtil {
 
 	public static List<Pessoa> formatarBairro(List<Pessoa> pessoas, List<Map<String, String>> enderecosViaCep) {
 		for (int i = 0; i < pessoas.size(); i++) {
-			if (pessoas.get(i).getBairro().equalsIgnoreCase("AEROPORTO") || pessoas.get(i).getBairro().equalsIgnoreCase("CENTRO")
+			if ((pessoas.get(i).getBairro().equalsIgnoreCase("AEROPORTO")
+					|| pessoas.get(i).getBairro().equalsIgnoreCase("CENTRO")
 					|| pessoas.get(i).getBairro().equalsIgnoreCase("OUTROS") || pessoas.get(i).getBairro() == null
-					|| pessoas.get(i).getBairro().isEmpty()) {
+					|| pessoas.get(i).getBairro().isEmpty())
+					&& (enderecosViaCep.get(i) != null && !enderecosViaCep.get(i).isEmpty())) {
 				pessoas.get(i).setBairro(enderecosViaCep.get(i).get("bairro"));
-			}	
+			}
 		}
-		
+
 		return pessoas;
 	}
 
@@ -63,40 +66,48 @@ public class PessoaUtil {
 		return listaDeCpf;
 	}
 
+	public static List<String> toList(String cpfs) {
+		cpfs = cpfs.replaceAll("'", "");
+		List<String> listaDeCpf = new ArrayList<>(Arrays.asList(cpfs.split(",")));
+		return listaDeCpf;
+	}
+
 	public static List<Map<String, String>> formatarJsonViaCep(List<String> enderecosJson) {
 		List<Map<String, String>> enderecosFormatados = new ArrayList<>();
-		
-		for(String jsonEndereco : enderecosJson) {
-			if(jsonEndereco == null) {
+
+		for (String jsonEndereco : enderecosJson) {
+			if (jsonEndereco == null) {
 				enderecosFormatados.add(null);
 			} else {
 				Map<String, String> endereco = new HashMap<>();
-				
+
 				Matcher matcher = Pattern.compile("\"\\D.*?\": \".*?\"").matcher(jsonEndereco);
-				
+
 				while (matcher.find()) {
 					String[] group = matcher.group().split(":");
 					endereco.put(group[0].replaceAll("\"", "").trim(), group[1].replaceAll("\"", "").trim());
 				}
-				
-				enderecosFormatados.add(endereco);			
+
+				enderecosFormatados.add(endereco);
 			}
 		}
-		
+
 		return enderecosFormatados;
 	}
 
 	public static List<Pessoa> formatarCep(List<Pessoa> pessoas) {
 		for (Pessoa pessoa : pessoas) {
 			pessoa.setCep(pessoa.getCep().replaceAll("\\R", "").trim());
-			if (pessoa.getCep().replace("-", "").length() >= 7 && pessoa.getCep().replace("-", "").length() < 10) {
-				String cep = pessoa.getCep().replace("-", "");
-				if (cep.length() == 7) {
-					cep = "0" + cep;
+			if (!pessoa.getCep().equals("ERRO CPF")) {
+				if (pessoa.getCep().replace("-", "").length() >= 7 && pessoa.getCep().replace("-", "").length() < 10) {
+					String cep = pessoa.getCep().replace("-", "");
+					if (cep.length() == 7) {
+						cep = "0" + cep;
+					}
+					pessoa.setCep(cep.substring(0, 5) + "-" + cep.substring(5));
+				} else {
+					pessoa.setMensagemErro("Cep inválido!");
 				}
-				pessoa.setCep(cep.substring(0, 5) + "-" + cep.substring(5));
-			} else {
-				pessoa.setMensagemErro("Cep inválido!");
 			}
 		}
 		return pessoas;
